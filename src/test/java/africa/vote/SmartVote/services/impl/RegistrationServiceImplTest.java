ee2611e3f34cac8b72e2e6c6b85a3db2d542f1d9
James <adulojujames@gmail.com>
2023-03-09 13:09:32 +0100

    test files added

---
+++ src/test/java/africa/vote/SmartVote/services/impl/RegistrationServiceImplTest.java
@@ -0,0 +1,16 @@
+package africa.vote.SmartVote.services.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class RegistrationServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void register() {
+    }
+}
